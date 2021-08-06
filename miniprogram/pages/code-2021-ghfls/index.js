add91ab320a6ed7226606332b8a65683adf20131
桐仔 <litong100476@offcn.com>
2021-08-06 10:18:32 +0800

    feat:行测申论干货福利社;

--- miniprogram/pages/code-2021-ghfls/index.js
+++ miniprogram/pages/code-2021-ghfls/index.js
@@ -6,34 +6,41 @@ Page({
    */
   data: {
     title:"行测申论干货福利社",// 标题
-    imageUrl:"http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021-gwjz-rmyh-share.jpg",// 分享时显示的图片
+    imageUrl:"http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021-code-ghfls.jpg",// 分享时显示的图片
 
     QRcode:false,  //二维码是否弹窗   真-现  假-隐
-    suffix:'' //后缀  通过后缀不同，变化不同二维码咨询
+    suffix:'lX5VoC', //后缀  通过后缀不同，变化不同二维码咨询
+    // （初步设置没有对应二维码的后缀放置长春小管家二维码）
+    imgalist:[
+      "http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021code/lX5VoC.jpg"
+    ]
   },
-  //点击弹出二维码部分内容
-  QRcode() {  
-    this.setData({
-      QRcode: true
-    })
-  },
-  // 关闭二维码部分内容
-  close(){
-    this.setData({
-      QRcode: false
-    })
-  },
+
+ /** 
+  * 预览图片 
+  */
+ previewImage: function (e) { 
+  var current=e.target.dataset.src; 
+  wx.previewImage({ 
+   current: current, // 当前显示图片的http链接 
+   urls: this.data.imgalist // 需要预览的图片http链接列表 
+  }) 
+ } ,
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
     // 获取后缀
     if (typeof options.scene !== "undefined") {
       this.setData({
-        suffix: options.scene
+        suffix: options.scene,
+        "imgalist[0]":"http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021code/"+options.scene+".jpg"
       })
     }
+    // 动态设置当前页面的标题
+    wx.setNavigationBarTitle({
+      title: this.data.title
+    })
   },
 
   /**
