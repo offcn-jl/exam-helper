66a3f9423d932a99f85ee3ce45b606537db98e68
桐仔 <litong100476@offcn.com>
2021-08-09 09:53:33 +0800

    fix:行测申论干货福利社添加判断后缀对应功能,修改图片链接;

--- miniprogram/pages/code-2021-ghfls/index.js
+++ miniprogram/pages/code-2021-ghfls/index.js
@@ -6,14 +6,16 @@ Page({
    */
   data: {
     title:"行测申论干货福利社",// 标题
-    imageUrl:"http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021-code-ghfls.jpg",// 分享时显示的图片
+    banner_bk:"http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021-code-ghfls-index.jpg",// 背景图片
+    imageUrl:"http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021-code-ghfls-share.jpg",// 分享时显示的图片
 
-    QRcode:false,  //二维码是否弹窗   真-现  假-隐
-    suffix:'lX5VoC', //后缀  通过后缀不同，变化不同二维码咨询
-    // （初步设置没有对应二维码的后缀放置长春小管家二维码）
+    suffix:'cBlzJ8', //后缀  通过后缀不同，变化不同二维码咨询
+    // （初步设置没有对应二维码的后缀放置省级二维码）
     imgalist:[
-      "http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021code/lX5VoC.jpg"
-    ]
+      "http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021code/cBlzJ8.jpg"
+    ],
+    // 判断后缀与列表中后缀一致，才有二维码，其余后缀显示省级二维码（宣传方面只有这几个地市后缀，添加此功能是为了预防突发状况）
+    codelist:["cBlzJ8","9796eU","b2ieZX","bpnm3","ck7M6r","cuAqQy","diBhaY","diGS6l","div7rM","dj42Px","djmVpr","dkIirz","dkjX8p","dkqacJ","dllDKK","dmx5Wy","eBbq26","eFPq6C","eGjREK","eynAg3","eyUz7F","ezbVb2","ezy7Tp","fE534Q","fEEFDi","fnVYBH","gWw8tb","gX25cd","h17IWN","hO0ywi","hsQVxb","ht54zE","htGlHB","hvEtbX","lX5VoC"]
   },
 
  /** 
@@ -32,6 +34,11 @@ Page({
   onLoad: function (options) {
     // 获取后缀
     if (typeof options.scene !== "undefined") {
+      // 判断后缀是否对应（宣传方面只有这几个地市后缀，添加此功能是为了预防突发状况）
+      // 如果后缀不对应(即后缀在表里位置为负数)，显示省级后缀
+      if(this.data.codelist.indexOf(options.scene)<0){
+        options.scene = "cBlzJ8"
+      }
       this.setData({
         suffix: options.scene,
         "imgalist[0]":"http://jl.offcn.com/zg/ty/images/exam-helper-mini-program/position/2021/2021code/"+options.scene+".jpg"
